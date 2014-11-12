c26e0c2eb503894b1f66808c1850b5243295cf1c
cwhitten1 <cwhitten@ufl.edu>
2014-11-12 14:19:21 -0500

    Fixed merge conflicts

--- Android Code/dancemarathon/src/com/example/dancemarathon/SwipeActivity.java
+++ Android Code/dancemarathon/src/com/example/dancemarathon/SwipeActivity.java
@@ -13,11 +13,9 @@ import android.os.Bundle;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.util.TypedValue;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 
 public class SwipeActivity extends ActionBarActivity
 {
@@ -129,46 +127,7 @@ public class SwipeActivity extends ActionBarActivity
 			return null;
 		}
 	}
-<<<<<<< HEAD
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment
-	{
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		private static final String ARG_SECTION_NUMBER = "section_number";
-
-		/**
-		 * Returns a new instance of this fragment for the given section number.
-		 */
-		public static PlaceholderFragment newInstance(int sectionNumber)
-		{
-			PlaceholderFragment fragment = new PlaceholderFragment();
-			Bundle args = new Bundle();
-			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		public PlaceholderFragment()
-		{
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState)
-		{
-			View rootView = inflater.inflate(R.layout.fragment_swipe,
-					container, false);
-			return rootView;
-		}
-
-	}
-	
 	public void openBrowser(View view){
 
 	    // Get url from tag
@@ -183,6 +142,4 @@ public class SwipeActivity extends ActionBarActivity
 
 	    startActivity(intent);
 	}
-=======
->>>>>>> FETCH_HEAD
 }
