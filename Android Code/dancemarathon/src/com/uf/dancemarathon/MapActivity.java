66ee9429541a68ab558db29b44a7ba50a450b2cd
cwhitten1 <cwhitten@ufl.edu>
2015-02-12 16:25:19 -0500

    Created map activity
    It is not added to UI yet

---
+++ Android Code/dancemarathon/src/com/uf/dancemarathon/MapActivity.java
@@ -0,0 +1,44 @@
+package com.uf.dancemarathon;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
+
+public class MapActivity extends Activity {
+
+	private ImageView mMap;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
+		
+		mMap = (ImageView) findViewById(R.id.map_image);
+		float mapX = mMap.getX();
+		float mapY = mMap.getY();
+	}
+		
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.map, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
