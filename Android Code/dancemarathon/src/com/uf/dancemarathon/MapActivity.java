aa44189bc169f4c93e8ef9df8a3b85ab80f4ebbf
cwhitten1 <cwhitten@ufl.edu>
2015-02-18 12:20:25 -0500

    Brought in open source code for map view
    The TouchImageView handles almost everything you could need for zooming,
    panning, and other gesture detection.

--- Android Code/dancemarathon/src/com/uf/dancemarathon/MapActivity.java
+++ Android Code/dancemarathon/src/com/uf/dancemarathon/MapActivity.java
@@ -3,18 +3,16 @@ package com.uf.dancemarathon;
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.graphics.Matrix;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.Toast;
 
 public class MapActivity extends Activity {
 
-	private FramedImageView mMap;
+	private TouchImageView mMap;
 	private ArrayList<ImageFrame> frames;
 	
 	   
@@ -23,26 +21,16 @@ public class MapActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
 		
-		mMap = (FramedImageView) findViewById(R.id.map_image);
+		mMap = (TouchImageView) findViewById(R.id.map_image);
 		
 		initFrames();
-		mMap.setFrames(frames);
+		mMap.setOnTouchListener(new MyTouchListener());
 		
 	}
-		
+	
 	private void initFrames()
 	{
-		TouchFrame f1 = new TouchFrame(0.2, 0.2, 0.7, 0.7);
-		f1.setOnFrameTouchListener(new OnFrameTouchListener(){
-
-			@Override
-			public void onFrameTouch(View v) {
-				// TODO Auto-generated method stub
-				makeFrameToast();
-			}
-			
-		});
-		
+		ImageFrame f1 = new ImageFrame(0.0, 0.0, 1.0, 0.5);
 		
 		frames = new ArrayList<ImageFrame>();
 		
@@ -74,5 +62,35 @@ public class MapActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private void handleTouchesForFrames(float x, float y, float imgWidth, float imgHeight)
+	{
+		for(int i = 0; i < frames.size(); i++)
+		{
+			ImageFrame f = frames.get(i);
+			
+			if(f.isPointInFrame(x, y,imgWidth, imgHeight))
+				makeFrameToast();
+			
+		}
+	}
+	
+	private class MyTouchListener implements View.OnTouchListener{
+
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			if(event.getAction() == MotionEvent.ACTION_UP)
+			{
+				float imgH = v.getHeight();
+				float imgW = v.getWidth();
+				float x = event.getX();
+				float y = event.getY();
+				
+				handleTouchesForFrames(x, y, imgW, imgH);
+			}
+			return true;
+		}
+		
 	}
 }
