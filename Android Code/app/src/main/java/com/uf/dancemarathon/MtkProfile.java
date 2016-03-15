a3d1c125b8af7f37372abec1d3916b684634bbaa
cwhitten1 <cwhitten@ufl.edu>
2016-03-15 00:34:12 -0400

    Update MTKProfile UI

--- Android Code/app/src/main/java/com/uf/dancemarathon/MtkProfile.java
+++ Android Code/app/src/main/java/com/uf/dancemarathon/MtkProfile.java
@@ -3,18 +3,23 @@ package com.uf.dancemarathon;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 /**
@@ -23,7 +28,10 @@ import android.widget.TextView;
  */
 public class MtkProfile extends AppCompatActivity
 {
-	Kid kid;
+	private Kid kid;
+    private Button titleButton;
+
+    private String ACTION_BAR_TITLE = "Meet The Kids";
 
 	public MtkProfile()
 	{
@@ -38,27 +46,14 @@ public class MtkProfile extends AppCompatActivity
 		//Get user from intent
 		Kid kid = getIntent().getExtras().getParcelable("kid");
 		this.kid = kid;
-		
-		setFields(kid);
-		
-		final TextView mile = (TextView) findViewById(R.id.milestone);
-		mile.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v) {
-				openLink((String)mile.getTag());
-				
-			}
-			
-		});
-		
-		//Set action bar title and color
+
+        setFields(kid);
+
+
+		//Customize action bar
 		ActionBar bar = getSupportActionBar();
-		bar.setTitle(kid.getName());
-		
-		int color = getResources().getColor(R.color.dm_orange_secondary);
-		ColorDrawable cd = new ColorDrawable();
-		cd.setColor(color);
-		bar.setBackgroundDrawable(cd);
+		TextView customBar = ActionBarUtility.customizeActionBar(this, bar, R.color.action_bar_color, R.color.White, Gravity.CENTER, 20, ACTION_BAR_TITLE);
+		FontSetter.setFont(this, FontSetter.fontName.ALTB, customBar);
 	}
 	
 	public static MtkProfile newInstance()
@@ -71,20 +66,65 @@ public class MtkProfile extends AppCompatActivity
 	{
 		ImageView pic  = (ImageView) findViewById(R.id.kid_pic);
 		TextView story = (TextView) findViewById(R.id.kid_story);
-		TextView mile = (TextView) findViewById(R.id.milestone);
-		
-		pic.setImageResource(kid.getImageId(this));
-		story.setText(kid.getStory());
-		
-		if (kid.getYoutube_id().length() > 0)
-		{
-			mile.setTag(kid.getYoutube_id());
-			mile.setText("View " + kid.getName() + "'s Milestone");
-		}
-		
-		FontSetter.setFont(this, FontSetter.fontName.AGBReg, story);
+        FontSetter.setFont(this, FontSetter.fontName.AGBReg, story);
+
+        pic.setImageResource(kid.getImageId(this));
+
+        story.setText(kid.getStory());
+        FontSetter.setFont(this, FontSetter.fontName.ALTL, story);
+
+        setupStoryPanel();
+        setupTitleButton();
 	}
-	
+
+    private void setupTitleButton(){
+        titleButton = (Button) findViewById(R.id.kid_title_button);
+
+        final String milestone_id = kid.getYoutube_id();
+        if (kid.hasMilestone())
+        {
+            titleButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openLink(milestone_id);
+
+                }
+
+            });
+        }
+        else
+         titleButton.setClickable(false);
+
+        setTitleButtonText(kid.hasMilestone());
+    }
+    private void setTitleButtonText(boolean hasMilestone){
+        String name = kid.getName();
+
+        if(hasMilestone){
+            titleButton.setText("Watch\n" + name + "'s\nMilestone");
+            titleButton.setPaintFlags(titleButton.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+            FontSetter.setFont(this, FontSetter.fontName.P, titleButton);
+        }
+        else{
+            titleButton.setText(name);
+            FontSetter.setFont(this, FontSetter.fontName.P, titleButton);
+        }
+    }
+
+    private void setupStoryPanel(){
+        if(!kid.hasStory()){
+
+            //Clear out all views from the story scroll view
+            ScrollView scroll = (ScrollView) findViewById(R.id.story_scroll_view);
+            scroll.removeAllViews();
+
+            //Add NoStoryAvailableFragment
+            FragmentManager fm = getSupportFragmentManager();
+            fm.beginTransaction().replace(R.id.story_scroll_view, NoStoryAvailableFragment.newInstance()).commit();
+        }
+    }
+
+
 	public void openLink(String youtubeId)
 	{
 	    //Log.d("tag", youtubeId);
@@ -107,4 +147,5 @@ public class MtkProfile extends AppCompatActivity
 			return new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=" + id));
 		}
 	}
+
 }
